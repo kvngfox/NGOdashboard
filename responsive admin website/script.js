03b67237fb6e81ced53e1299cfb5d7b8519b54b4
kvngfox <75882581+kvngfox@users.noreply.github.com>
2022-09-05 23:05:58 +0100

    Add files via upload

---
+++ responsive admin website/script.js
@@ -0,0 +1,10 @@
+var sideMenu = document.getElementById("aside");
+
+
+function showMenu() {
+    sideMenu.style.display = "block"
+}
+
+function hideMenu() {
+    sideMenu.style.display = "none"
+}
